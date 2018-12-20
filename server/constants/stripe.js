241fb7cd2367c1956cf4297bbd2c4090438d82e1
Alex Le <alexle512@gmail.com>
2018-12-20 11:30:14 -0600

    moved around file structure

--- server/constants/stripe.js
+++
@@ -1,9 +0,0 @@
-const configureStripe = require('stripe');
-
-const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-    ? 'sk_test_2JcJpVQEDBKZzcQxjBvWZJrf'
-    : 'sk_test_2JcJpVQEDBKZzcQxjBvWZJrf';
-
-const stripe = configureStripe(STRIPE_SECRET_KEY);
-
-module.exports = stripe;
